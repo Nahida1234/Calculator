89154c9d4e90a5d1e8ab55ba20c7d64947ed4f2f
nahida1234 <nahida_036@yahoo.com>
2014-09-27 13:15:37 +0600

    Calculator App

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatorApp
+{
+    class Calculator
+    {
+  
+
+        public double add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public double subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        public double multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public double divide(double firstNumber, double secondNumber)
+        {
+            return  firstNumber / secondNumber;
+        }
+
+
+    }
+
+
+
+}
---
+++ CalculatorFormUI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorApp
+{
+    public partial class CalculatorFormUI : Form
+    {
+        public CalculatorFormUI()
+        {
+            InitializeComponent();
+        }
+        Calculator calculator=new Calculator();
+         double firstNumber = 0;
+         double secondNumber = 0; 
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = calculator.add(firstNumber, secondNumber);
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = calculator.subtract(firstNumber, secondNumber);
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = calculator.multiply(firstNumber, secondNumber);
+            resultTextBox.Text = result.ToString();
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+            double result = calculator.divide(firstNumber, secondNumber);
+            resultTextBox.Text = result.ToString();
+        }
+
+    }
+}
